949ef7ab21fa56781611e11c1765c53600051a93
Foodie <olaorefouad565@gmail.com>
2020-05-07 17:35:26 +0100

    feature: configured the splash screen and setup navigation to the onboarding section

---
+++ app/src/main/java/dev/goteam/paydrift/activities/AuthActivity.java
@@ -0,0 +1,16 @@
+package dev.goteam.paydrift.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dev.goteam.paydrift.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
---
+++ app/src/main/java/dev/goteam/paydrift/fragments/SplashScreenFragment.java
@@ -0,0 +1,53 @@
+package dev.goteam.paydrift.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.navigation.NavigationView;
+
+import dev.goteam.paydrift.R;
+
+public class SplashScreenFragment extends Fragment {
+
+    private Handler handler;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // check if this is the first time the user opens the app - preferences would be used.
+                if (isFirstTime()) {
+                    NavHostFragment.findNavController(
+                            SplashScreenFragment.this).navigate(SplashScreenFragmentDirections.actionSplashScreenFragmentToOnboardingFragment()
+                    );
+                } else {
+                    // add logic to navigate to the login screen
+                }
+            }
+        }, 2500);
+    }
+
+    private boolean isFirstTime() {
+        return true;
+    }
+
+}
