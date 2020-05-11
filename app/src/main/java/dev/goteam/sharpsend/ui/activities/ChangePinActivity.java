3d8fb30ec86e75cc4bd59a3f7511eb08c01a83b5
Foodie <olaorefouad565@gmail.com>
2020-05-11 22:52:05 +0100

    feature: added forgot pin, change pin and fingerprint support

---
+++ app/src/main/java/dev/goteam/sharpsend/ui/activities/ChangePinActivity.java
@@ -0,0 +1,85 @@
+package dev.goteam.sharpsend.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import dev.goteam.sharpsend.R;
+import dev.goteam.sharpsend.databinding.ActivityChangePinBinding;
+import dev.goteam.sharpsend.utils.Prefs;
+
+public class ChangePinActivity extends AppCompatActivity implements TextWatcher {
+
+    private ActivityChangePinBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_change_pin);
+
+        binding.currentPinField.getEditText().addTextChangedListener(this);
+        binding.newPinField.getEditText().addTextChangedListener(this);
+
+        binding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        binding.changeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String currentPIN = binding.currentPinField.getEditText().getText().toString();
+                String newPIN = binding.newPinField.getEditText().getText().toString();
+
+                boolean changed = Prefs.changePIN(getApplicationContext(), currentPIN, newPIN);
+                if (changed) {
+                    Toast.makeText(ChangePinActivity.this, "Changing PIN....", Toast.LENGTH_SHORT).show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(ChangePinActivity.this, "PIN Changed", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }, 1250);
+                } else {
+                    Snackbar.make(view, "Wrong PIN Supplied", Snackbar.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        String currentPIN = binding.currentPinField.getEditText().getText().toString();
+        String newPIN = binding.newPinField.getEditText().getText().toString();
+        if (!currentPIN.isEmpty() && !newPIN.isEmpty() && currentPIN.length() == 4 && newPIN.length() == 4 && !currentPIN.equals(newPIN)) {
+            binding.changeButton.setEnabled(true);
+        } else {
+            binding.changeButton.setEnabled(false);
+        }
+
+        if (currentPIN.equals(newPIN)) {
+            Toast.makeText(this, "You cannot provide the same pin to be changed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    }
+}
--- app/src/main/java/dev/goteam/sharpsend/utils/Prefs.java
+++ app/src/main/java/dev/goteam/sharpsend/utils/Prefs.java
@@ -8,11 +8,33 @@ public class Prefs {
     private static String AUTHENTICATED = "authenticated";
     private static String PREFS_NAME = "sharpsend_prefs";
     private static String USERNAME_KEY = "username";
+    private static String FINGERPRINT_KEY = "fingerprint";
     private static String PIN_KEY = "pin";
 
     public static boolean isAuthenticated(Context context) {
         SharedPreferences sharedPreferences = getPreferences(context);
         return sharedPreferences.getBoolean(AUTHENTICATED, false);
+    }
+
+    public static boolean changePIN(Context ctx, String oldPIN, String newPIN) {
+        boolean changed = false;
+        SharedPreferences sharedPreferences = getPreferences(ctx);
+
+        String actualPIN = sharedPreferences.getString(PIN_KEY, "");
+        if (actualPIN.equals(oldPIN)) {
+            setPIN(ctx, newPIN);
+            changed = true;
+        }
+
+        return changed;
+    }
+
+    public static void setPIN(Context ctx, String newPIN) {
+        SharedPreferences sharedPreferences = getPreferences(ctx);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(PIN_KEY, newPIN);
+        editor.commit();
     }
 
     public static String[] getUserData(Context context) {
@@ -36,6 +58,21 @@ public class Prefs {
         editor.putString(PIN_KEY, PIN);
 
         editor.putBoolean(AUTHENTICATED, true);
+
+        editor.apply();
+    }
+
+    public static boolean isFingerprintEnabled(Context ctx) {
+        SharedPreferences sharedPreferences = getPreferences(ctx);
+
+        return sharedPreferences.getBoolean(FINGERPRINT_KEY, false);
+    }
+
+    public static void setFingerprintState(Context context, boolean state) {
+        SharedPreferences sharedPreferences = getPreferences(context);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(FINGERPRINT_KEY, state);
 
         editor.apply();
     }
